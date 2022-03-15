8c3344550a0986e293ccd44bace1adaaff5837ee
privewrld <qutetutorials@gmail.com>
2022-03-15 19:37:58 +0100

    p2

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
+
+const welcomeChannelId = "953354533684256800"
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("messageCreate", (message) =>{
-    if (message.content = "hi"){
-        message.reply("Hello World!")
-    }
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
